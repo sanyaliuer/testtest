0935e7c72c816eaa7a56ff06d4113015ec4e0ff6
sanyaliuer <130228366+sanyaliuer@users.noreply.github.com>
2023-04-09 23:31:27 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,10 +8,10 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 
-- 特感谢 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
+- 特别感谢 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
 
 - 本站由 [三亚柳儿]搭建。
 
-- 想要部署的朋友，可关注公众号：**PLATO对话**，添加我个人微信获取保姆级部署文档，
+- 想要更好地使用的朋友，可上面二维码加入微信群，或关注公众号：三亚亲子圈，或添加我个人微信sanyaliuer，获取高级使用指南。
 
 - **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
